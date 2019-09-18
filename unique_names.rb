12abed00c088b4b870b6fd483069272929cc6ac9
ferley <fleon@md4u.com.co>
2019-09-18 13:43:49 -0500

    Se adicinaron puts

--- unique_names.rb
+++ unique_names.rb
@@ -1,5 +1,8 @@
 class UniqueNames
   def unique_name(names1, names2)
+    p "names1: #{names1}"
+    p "names2: #{names2}"
+    p "resultado: #{(names1 + names2).uniq}"
     return (names1 + names2).uniq
   end
 end
